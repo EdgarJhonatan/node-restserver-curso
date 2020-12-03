d666138048c4d4abd09056ca468e781a9bcbd760
Edgar Delgadillo <jdmkrd@gmail.com>
2020-12-03 12:45:09 -0500

    RestServer -usario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 //puerto 
 //======
 process.env.PORT = process.env.PORT || 3000
+
+//======
+//Entorno 
+//======
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//======
+//BD 
+//======
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://edgar:dylan1991$@cluster0.xiroh.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
